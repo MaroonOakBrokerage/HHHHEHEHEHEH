b6f2913916f5e2bf1fbe68b65249a6d019679ef6
MaroonOakBrokerage <cole.t.hindes@outlook.com>
2026-07-12 14:46:33 -0500

    Fix Next.js dependency versions

--- Maroon-Oak-Brokerage-Website-(Next.js).zip/next.config.ts
+++ Maroon-Oak-Brokerage-Website-(Next.js).zip/next.config.ts
@@ -18,6 +18,11 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "unsplash.com",
+        pathname: "/photos/**",
+      },
+      {
+        protocol: "https",
         hostname: "plus.unsplash.com",
         pathname: "/**",
       },
